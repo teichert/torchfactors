407020f93daf3603990141a4586dc31df4bce81a
Adam Teichert <adam.teichert@snow.edu>
2021-07-19 15:57:38 -0600

    coverage on linear_factor.py

--- tests/test_linear_factor.py
+++ tests/test_linear_factor.py
@@ -33,3 +33,12 @@ def test_bad_linear_factor():
     v = TensorVar(Range(10), torch.zeros(10))
     with pytest.raises(ValueError):
         LinearFactor(m.namespace('root'), v, input=torch.ones(13, 10))
+
+
+def test_no_bias_no_input():
+    m = Model[Any]()
+    v = TensorVar(Range(7), torch.ones(10))
+    f = LinearFactor(m.namespace('root'), v, bias=False)
+    out = f.dense
+    expected = torch.zeros(10, 7)
+    assert out.allclose(expected)
